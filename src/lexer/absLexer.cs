95a95a1959744ed9530a2db3893a5ff1ace5a204
Danny F. Pires <danny.f.pires@hofstra.edu>
2021-10-22 10:57:39 -0400

    Lexical Analyzer dll added with makefile formula

---
+++ src/lexer/absLexer.cs
@@ -0,0 +1,13 @@
+public interface absLexer
+{
+   lexToken next(); // returns null at eof
+   int linenum();
+}
+
+public class lexToken
+{
+   public string token_type;
+   public object token_value;
+   public lexToken(string t, object v) {token_type=t; token_value=v;}
+   public override string ToString() {return token_type+"("+token_value+")";}
+}
