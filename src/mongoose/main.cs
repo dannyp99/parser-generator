521797d51e2edde3aaa2b96027dabdc324c15ce0
Daniel Dimijian <jetfrog28@yahoo.com>
2021-12-06 20:20:12 -0500

    fix indentation

--- src/mongoose/main.cs
+++ src/mongoose/main.cs
@@ -1,17 +1,19 @@
 using System; //consoles
 using static FSEvaluator; //needed for expr
+using static MongooseCompiler;
 class Driver {
   public static void Main(string[] argv){
     string srcfile = "./test1.ms";
     if(argv.Length >= 1) { srcfile = argv[0]; }
-      var Par = Generator.make_parser(); 
-      if(Par != null) {
-        expr t = (expr)Par.Parse(new MongooseLexer(srcfile, "EOF"));
-        if(t != null) {
-            //FSPrint(t);
-            // run(t);
-            Console.WriteLine("Result: "+t); 
-        }
+    var Par = Generator.make_parser(); 
+    if(Par != null) {
+      expr t = (expr)Par.Parse(new MongooseLexer(srcfile, "EOF"));
+      if(t != null) {
+        //FSPrint(t);
+        // run(t);
+        compile(t);
+        Console.WriteLine("Result: "+t); 
       }
+    }
   }
 }
